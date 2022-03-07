4c73bb3b3aae5af56ccad3e956a5f7cd11ab85ca
YarikDrovolsky <yar.drovolsky@gmail.com>
2022-03-07 17:46:03 +0200

    Added unit test for inserting and getting items from MovesHistory.
    Using CppUnitTestFramework
    
    When tried to build test bumped into LNK error: unresolved external symbols of Figure and MovesHistory methods.
    Found a solution: https://docs.microsoft.com/uk-ua/previous-versions/visualstudio/visual-studio-2015/test/unit-testing-existing-cpp-applications-with-test-explorer?view=vs-2015&redirectedfrom=MSDN#objectRef. But it doesn't work and the smae errors.
    So I simply #include cpp-files with implementations of Figure and MovesHistotry methods.
    There is explanation why it works: https://stackoverflow.com/questions/47515089/lnk2019-unresolved-external-symbol-in-vs-unit-testing
    
    Solution for second problem (I replaced Assert::AreEqual() by Assert::IsTrue())
    https://stackoverflow.com/questions/28739852/c2338-compile-error-for-a-microsoft-visual-studio-unit-test
    
    Useful about unit-tests:
    https://docs.microsoft.com/en-us/visualstudio/test/writing-unit-tests-for-c-cpp?view=vs-2022 - guide
    
    https://docs.microsoft.com/en-us/visualstudio/test/microsoft-visualstudio-testtools-cppunittestframework-api-reference?view=vs-2022 - API of CppUnitTestFramework
    
    https://docs.microsoft.com/ru-ru/visualstudio/test/writing-unit-tests-for-c-cpp?view=vs-2022 - one more guide

---
+++ GameUnitTests/GameUnitTests.cpp
@@ -0,0 +1,75 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+#include "../lab_3_project/MovesHistory.h"
+#include "../lab_3_project/MovesHistory.cpp"
+#include "../lab_3_project/Figure.cpp"
+#include "../lab_3_project/Point.cpp"
+
+
+#include <random>
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace GameUnitTests
+{
+	TEST_CLASS(MovesHistoryTests)
+	{
+	public:
+		
+		TEST_METHOD(Test)
+		{
+			MovesHistory arr;
+			std::vector<Move> moves;
+			std::vector<Figure*> removed_figures;
+
+			std::default_random_engine gen;
+			std::uniform_int_distribution<int> dis(0, 7);
+
+			std::size_t N = 100; // number of inserted items
+			// insert items
+			for (std::size_t i = 0; i < N; i++) {
+				Move move{ Point{dis(gen), dis(gen)}, Point{dis(gen), dis(gen)} };
+				Figure* fig = new Figure{ Color::black, FigType::rook, Point{dis(gen), dis(gen)} };
+
+				arr.insert(move, fig);
+				moves.push_back(move);
+				removed_figures.push_back(fig);
+
+				Assert::AreEqual(i, arr.getCurIndex());
+				Assert::AreEqual(false, arr.isEmpty());
+				Assert::AreEqual(moves.size(), arr.getSize());
+			}
+
+			for (std::size_t i = 0; i < N; i++) {
+				Assert::AreEqual(N - i, arr.getSize());
+				Assert::AreEqual(N - 1 - i, arr.getCurIndex());
+				
+				auto result = arr.undoMove();
+				Move move = result.first;
+				Figure* fig = result.second;
+
+				
+				Assert::IsTrue(moves[N - 1 - i] == move);
+				Assert::IsTrue(removed_figures[N - 1 - i] == fig);
+				
+			//	Assert::AreEqual(moves[N - 1 - i], move);
+			//	Assert::AreEqual(removed_figures[N - 1 - i], fig);
+
+				if (i == N - 1) { // end, container must be empty
+					Assert::AreEqual(true, arr.isEmpty());
+				}
+				else {
+					Assert::AreEqual(false, arr.isEmpty());
+					Assert::AreEqual(N - i, arr.getSize() + 1);
+					Assert::AreEqual(N - 1 - i, arr.getCurIndex() + 1);
+				}	
+			}
+
+			// need to clean allocated memory
+			for (std::size_t i = 0; i < N; i++) {
+				delete removed_figures[i];
+			}
+		}
+	};
+}
